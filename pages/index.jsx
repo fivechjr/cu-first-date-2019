59ae0344a26f4379b06bf8907633ed3064a77c2e
FiveChjr <f5.chjr@gmail.com>
2019-06-14 10:11:19 +0700

    feat: initialize project

---
+++ pages/index.jsx
@@ -0,0 +1,15 @@
+import { Fragment } from 'react'
+import Head from 'next/head'
+
+function Home() {
+    return (
+        <Fragment>
+            <Head>
+                <title>CU First Date 2019</title>
+            </Head>
+            <p className="text-red-600">Hello!</p>
+        </Fragment>
+    )
+}
+
+export default Home
