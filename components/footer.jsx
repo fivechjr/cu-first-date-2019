b735789856b5a6fc2fb2c6cfd211219fbae8d238
FiveChjr <f5.chjr@gmail.com>
2019-06-18 12:10:27 +0700

    fix(components): reduce font size in `footer.jsx`

--- components/footer.jsx
+++ components/footer.jsx
@@ -3,7 +3,7 @@ import View from './view'
 const Footer = () => (
     <footer className="bg-white">
         <View className="py-10">
-            <p className="text-center font-serif text-xs uppercase tracking-widest text-gray-600">
+            <p className="text-center font-sans text-2xs uppercase tracking-widest text-gray-600">
                 <span>&copy; CU First Date {new Date().getFullYear()}</span>
                 <span className="opacity-50"> &middot; Patcharapat Chaijaroen</span>
             </p>
