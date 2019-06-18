6cd8f116c3cb9bbe179e4ea117df0c5a08320564
FiveChjr <f5.chjr@gmail.com>
2019-06-18 11:49:30 +0700

    fix(components): fix layout in `clubs.jsx`

--- components/sections/clubs.jsx
+++ components/sections/clubs.jsx
@@ -3,22 +3,24 @@ import SectionTitle from '../section-title'
 import View from '../view'
 import ChulaEngineeringIcon from '../chula-engineering-icon'
 
-const Club = ({ name }) => (
-    <div className="lg:w-1/4 md:w-1/3 w-1/2 px-2 my-2">
-        <div className="flex flex-col bg-white shadow-2xl rounded h-full">
-            <div
-                className="w-full rounded-tl rounded-tr bg-gray-300"
-                style={{
-                    height: '150px',
-                    backgroundImage: 'url("")',
-                    backgroundSize: 'cover',
-                    backgroundRepeat: 'no-repeat',
-                    backgroundPosition: 'center center'
-                }}
-            />
+const Club = ({ name, type }) => (
+    <div className="lg:w-1/4 md:w-1/2 w-full px-2 my-2">
+        <div className="flex flex-col bg-white shadow-2xl h-full">
+            <div className="pt-5 px-5 md:px-0 md:pt-0">
+                <div
+                    className="w-full bg-gray-300"
+                    style={{
+                        height: '150px',
+                        backgroundImage: 'url("")',
+                        backgroundSize: 'cover',
+                        backgroundRepeat: 'no-repeat',
+                        backgroundPosition: 'center center'
+                    }}
+                />
+            </div>
             <div className="flex w-full flex-col justify-start px-6 py-5">
                 <div className="font-sans text-2xs leading-none tracking-widest mb-1 text-gray-600 font-medium">
-                    CLUB
+                    {type ? type.toUpperCase() : 'CLUB'}
                 </div>
                 <p className="leading-relaxed font-headline text-gray-700">{name}</p>
             </div>
@@ -35,18 +37,18 @@ const Clubs = () => (
             <div className="container">
                 <div className="mx-8">
                     <div className="flex flex-wrap justify-center -my-2 -mx-2">
-                        <Club name="Thinc." />
-                        <Club name="วอลเลย์บอล" />
-                        <Club name="ยิงปืน" />
-                        <Club name="มวย" />
-                        <Club name="โต้วาที" />
-                        <Club name="รักบี้" />
-                        <Club name="FE Camp" />
-                        <Club name="กีฬาทางน้ำ" />
-                        <Club name="วิชาการ" />
-                        <Club name="ค่ายลานเกียร์" />
-                        <Club name="ผู้นำเชียร์" />
-                        <Club name="IMC (Intania Music Club)" />
+                        <Club type="Academic" name="Thinc." />
+                        <Club type="Sport" name="วอลเลย์บอล" />
+                        <Club type="Sport" name="ยิงปืน" />
+                        <Club type="Sport" name="มวย" />
+                        <Club type="Academic" name="โต้วาที" />
+                        <Club type="Sport" name="รักบี้" />
+                        <Club type="Academic" name="FE Camp" />
+                        <Club type="Sport" name="กีฬาทางน้ำ" />
+                        <Club type="Academic" name="วิชาการ" />
+                        <Club type="Academic" name="ค่ายลานเกียร์" />
+                        <Club type="Academic" name="ผู้นำเชียร์" />
+                        <Club type="Academic" name="IMC (Intania Music Club)" />
                     </div>
                 </div>
             </div>
