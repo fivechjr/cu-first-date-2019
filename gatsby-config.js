972cd583a7990b04b0932db5c09d20e9d7475bce
FiveChjr <f5.chjr@gmail.com>
2019-06-18 18:10:43 +0700

    build: test `gatsby build`

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
     siteMetadata: {
-        title: `Gatsby Default Starter`,
-        description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-        author: `@gatsbyjs`
+        title: `CU First Date 2019`,
+        description: `The Best Gift for You`,
+        author: ``
     },
     plugins: [
         `gatsby-plugin-react-helmet`,
@@ -24,7 +24,7 @@ module.exports = {
                 background_color: `#663399`,
                 theme_color: `#663399`,
                 display: `minimal-ui`,
-                icon: `src/images/gatsby-icon.png` // This path is relative to the root of the site.
+                icon: `static/favicon@2x.png` // This path is relative to the root of the site.
             }
         },
         `gatsby-plugin-postcss`
