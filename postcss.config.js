99a888242c555ee52941c057ec0ca636f815d58b
FiveChjr <f5.chjr@gmail.com>
2019-06-18 19:03:01 +0700

    build: build project (again) with `gatsby-plugin-purgecss`

--- postcss.config.js
+++ postcss.config.js
@@ -1,22 +1,3 @@
-// postcss.config.js
-const purgecss = require('@fullhuman/postcss-purgecss')({
-    // Specify the paths to all of the template files in your project
-    content: [
-        './src/**/*.jsx',
-        './src/**/**/*.jsx',
-        './src/**/*.js',
-        './src/**/**/*.js'
-        // etc.
-    ],
-
-    // Include any special characters you're using in this regular expression
-    defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-})
-
 module.exports = {
-    plugins: [
-        require('tailwindcss'),
-        require('autoprefixer'),
-        ...(process.env.NODE_ENV === 'production' ? [purgecss] : [])
-    ]
+    plugins: [require('tailwindcss'), require('autoprefixer')]
 }
