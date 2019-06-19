dbf707da51878e159845143effb63faf3d22b17c
FiveChjr <f5.chjr@gmail.com>
2019-06-19 16:28:37 +0700

    build: use `build.sh`

--- src/components/common/separator.jsx
+++ src/components/common/separator.jsx
@@ -1,15 +1,11 @@
 import React, { Fragment } from 'react'
 
 const Separator = ({ en, th, icon, color }) => {
-    const mainColor = color ? color.toLowerCase() : 'red'
     return (
         <Fragment>
             <div className="flex flex-row my-10">
                 <div className="flex mr-5" style={{ width: '40px' }} />
-                <div
-                    className={`flex border-b border-${mainColor}-200`}
-                    style={{ width: '50px' }}
-                />
+                <div className="flex border-b border-red-600" style={{ width: '50px' }} />
             </div>
         </Fragment>
     )
